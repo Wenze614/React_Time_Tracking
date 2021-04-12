9cc7b8eedea1e27961c392740e8e19e0ecb8d34c
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-04-12 17:00:02 +0800

    Adds reactstrap, added navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+
+import {
+  Navbar,
+  NavbarBrand,
+} from 'reactstrap';
 
 function App() {
   return (
     <React.Fragment>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Simple Timer Application</NavbarBrand>
+      </Navbar>
 
-      
+
+      <Button>Test</Button>
     </React.Fragment>
   );
 }

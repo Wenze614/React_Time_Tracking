e2617b39ddc706b73bfca22006d2aaf1d18515d7
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-04-19 14:42:01 +0800

    add initiate state in Timer.tsx

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -2,17 +2,32 @@ import React, {useState, useEffect} from 'react'
 
 const Timer = () =>{
     const [timer, setTimer] = useState(0);
-    
-    useEffect(() => {
-        console.log('CREATED')
-        let interval = setInterval(()=>{
-                setTimer(timer => timer+1)
-            },1000
-        )
+    const [initiate, setInitiate] = useState(false)
+    // useEffect(() => {
+    //     console.log('CREATED')
+    //     let interval = setInterval(()=>{
+    //             setTimer(timer => timer+1)
+    //         },1000
+    //     )
 
-        return () => clearInterval(interval)
-    }, [timer])
-    return <div>{timer}s</div>
+    //     return () => clearInterval(interval)
+    // }, [timer])
+    useEffect(()=>{
+        console.log(timer)
+
+    },[timer])   
+    useEffect(() => {
+        if (!initiate)
+        {
+            let myInterval = setInterval(() => {
+                setTimer(timer => timer+1)
+            },1000)
+            console.log(timer)
+            setInitiate(false)
+        }
+          
+    },[])
+return <div><div>{Math.floor(timer/3600)}h</div><div>{Math.floor(timer/60)}min</div><div>{timer%60}s</div></div>
 }
 
 export default Timer

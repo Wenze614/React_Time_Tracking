dc426c6d3d5f28eeb75dbae9bdefeffb7e052796
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-04-19 14:50:24 +0800

    Import Timer.css, and fixed setInitiate error

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,20 +1,11 @@
 import React, {useState, useEffect} from 'react'
-
+import "./Timer.css"
 const Timer = () =>{
     const [timer, setTimer] = useState(0);
     const [initiate, setInitiate] = useState(false)
-    // useEffect(() => {
-    //     console.log('CREATED')
-    //     let interval = setInterval(()=>{
-    //             setTimer(timer => timer+1)
-    //         },1000
-    //     )
-
-    //     return () => clearInterval(interval)
-    // }, [timer])
     useEffect(()=>{
         console.log(timer)
-
+    
     },[timer])   
     useEffect(() => {
         if (!initiate)
@@ -23,11 +14,11 @@ const Timer = () =>{
                 setTimer(timer => timer+1)
             },1000)
             console.log(timer)
-            setInitiate(false)
+            setInitiate(true)
         }
           
     },[])
-return <div><div>{Math.floor(timer/3600)}h</div><div>{Math.floor(timer/60)}min</div><div>{timer%60}s</div></div>
+return <div className = "timer-content" >{Math.floor(timer/3600)}h-{Math.floor(timer/60)}min-{timer%60}s</div>
 }
 
 export default Timer

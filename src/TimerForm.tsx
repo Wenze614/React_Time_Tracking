7da017346b816885ff78f1fda48b8ef871ca1e81
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-04-28 00:25:33 +0800

    move timer content into timerForm and delete Timer.tsx

--- src/TimerForm.tsx
+++ src/TimerForm.tsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import { Button } from 'react-bootstrap';
 import "./TimerForm.css"
-import Timer from './Timer'
 import "./Timer-input.css"
+import "./Timer.css"
 export interface TimerObject {
     job: string;
     id: number;
@@ -18,16 +18,27 @@ interface TimerFormProps {
 }
 
 const TimerForm = (props: TimerFormProps) => {
+    const [timer, setTimer] = useState(0);
     const handleDelete = (todo: TimerObject) =>{
         props.onDelete(todo)
     }
+    useEffect(() => {
+        
+            setInterval(() => {
+                setTimer(timer => timer+1)
+            },1000)
+    },[])
     return (<div className = "todoList">
         {props.todoList.map((todo)=>(
         <div key = {todo.id} className = "task-item">
         <div className = "id-content">{todo.id}</div>
         <div className = "job-content">{todo.job}</div>
-        <Timer/>
-        
+        <div className = "timer-content" >
+        <span className = "important-numbers">{Math.floor(timer/3600)}</span>h-
+        <span className = "important-numbers">{Math.floor(timer/60)}</span>min-
+        <span className = "important-numbers">{timer%60}</span>s</div>
+        <button className = "start-button" onClick={() => handleDelete(todo)} >Pause</button>
+        <button className = "start-button" onClick={() => handleDelete(todo)} >Resume</button>
         <button className = "start-button" onClick={() => handleDelete(todo)} >Delete</button>
         </div>
         ))}

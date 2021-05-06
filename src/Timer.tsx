3c6f198510fab2d2209ad125e6c944f16e8c9cbc
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-05-06 17:22:08 +0800

    fix bugs on timer and move clearInterval to button click

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -7,27 +7,28 @@ const Timer = (props:{todo: TimerObject}) =>{
     const dispatch = useAppDispatch()
     const isPaused = props.todo.status
     const this_ID = props.todo.id
+    const counter = props.todo.counter
+    var id : ReturnType<typeof setInterval>
     useEffect(() => {
         if (isPaused === false){
-            const id = setInterval(() => {
+            id = setInterval(() => {
                 dispatch(addCounter(this_ID))
             },1000)
-            return ()=>clearInterval(id)
         }
     },[isPaused])
 
     return  (<div key = {props.todo.id} className = "task-item">
                     <div className = "job-content">{props.todo.job}</div>
                     <div className = {!isPaused ? "timer-content":"timer-content paused"} >
-                        <span className = "important-numbers">{Math.floor(this_ID/3600)}</span>h &nbsp;
-                        <span className = "important-numbers">{Math.floor(((this_ID%3600))/60)}</span>min &nbsp;
-                        <span className = "important-numbers">{this_ID%60}</span>s</div>
+                        <span className = "important-numbers">{Math.floor(counter/3600)}</span>h &nbsp;
+                        <span className = "important-numbers">{Math.floor(((counter%3600))/60)}</span>min &nbsp;
+                        <span className = "important-numbers">{counter%60}</span>s</div>
                             {!isPaused ?
                                 (<button className="activate-button" onClick={()=>dispatch(pauseTimer(props.todo.id))}>Pause</button>)
                                 :
                                 (<button className="activate-button" onClick={()=>dispatch(resumeTimer(props.todo.id))}>Resume</button>)
                             }
-                    <button className="delete-button" onClick={()=>dispatch(deleteTimer(props.todo.id))}>X</button>
+                    <button className="delete-button" onClick={()=>{clearInterval(id);dispatch(deleteTimer(props.todo.id))}}>X</button>
                 </div>
                
     )

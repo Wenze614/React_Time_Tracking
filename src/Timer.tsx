99bfec73dbff1f31e45a5b678cde88e6d9bb72db
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-05-06 15:14:47 +0800

    add 'addCounter' reducer in timerSlice and delete timer state in Timer.tsx

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,16 +1,16 @@
-import {useState, useEffect} from 'react'
+import {useEffect} from 'react'
 import "./Timer.css"
 import {TimerObject}from './features/timer/timerSlice'
 import {useAppDispatch } from './app/hook'
-import {pauseTimer, resumeTimer, deleteTimer} from './features/timer/timerSlice'
+import {pauseTimer, resumeTimer, deleteTimer, addCounter} from './features/timer/timerSlice'
 const Timer = (props:{todo: TimerObject}) =>{
-    const [timer, setTimer] = useState(0);
     const dispatch = useAppDispatch()
     const isPaused = props.todo.status
+    const this_ID = props.todo.id
     useEffect(() => {
         if (isPaused === false){
             const id = setInterval(() => {
-                setTimer(timer => timer+1)
+                dispatch(addCounter(this_ID))
             },1000)
             return ()=>clearInterval(id)
         }
@@ -19,9 +19,9 @@ const Timer = (props:{todo: TimerObject}) =>{
     return  (<div key = {props.todo.id} className = "task-item">
                     <div className = "job-content">{props.todo.job}</div>
                     <div className = {!isPaused ? "timer-content":"timer-content paused"} >
-                        <span className = "important-numbers">{Math.floor(timer/3600)}</span>h &nbsp;
-                        <span className = "important-numbers">{Math.floor(((timer%3600))/60)}</span>min &nbsp;
-                        <span className = "important-numbers">{timer%60}</span>s</div>
+                        <span className = "important-numbers">{Math.floor(this_ID/3600)}</span>h &nbsp;
+                        <span className = "important-numbers">{Math.floor(((this_ID%3600))/60)}</span>min &nbsp;
+                        <span className = "important-numbers">{this_ID%60}</span>s</div>
                             {!isPaused ?
                                 (<button className="activate-button" onClick={()=>dispatch(pauseTimer(props.todo.id))}>Pause</button>)
                                 :

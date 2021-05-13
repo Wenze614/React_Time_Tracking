8c3a0078279b31de0eea6e875f8e3fa9540b00d9
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-05-13 14:40:21 +0800

    add css

--- src/Record.tsx
+++ src/Record.tsx
@@ -6,10 +6,10 @@ interface RecordProps {
 export const Record = (props:RecordProps) => {
     const counter = props.todo.counter
     return <div className = "record-item">
-                <div className = "job-content record">{props.todo.job}</div>
-                    <div className = "timer-content record" >
-                        <span className = "important-numbers">{Math.floor(counter/3600)}</span>h &nbsp;
-                        <span className = "important-numbers">{Math.floor(((counter%3600))/60)}</span>min &nbsp;
-                        <span className = "important-numbers">{counter%60}</span>s</div>
+                <div className = "record-job">{props.todo.job}</div>
+                    <div className = "record-time" >
+                        <span className = "record-time-number">{Math.floor(counter/3600)}</span>h &nbsp;
+                        <span className = "record-time-number">{Math.floor(((counter%3600))/60)}</span>min &nbsp;
+                        <span className = "record-time-number">{counter%60}</span>s</div>
          </div>
 }
